824a4498b888f899ab117223b3127d953387f640
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-22 12:52:30 -0500

    Added basic services/interfaces for pet clinic. Closes #5

---
+++ pet-clinic-data/src/main/java/austinspinazze/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package austinspinazze.springframework.sfgpetclinic.services;
+
+import austinspinazze.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
