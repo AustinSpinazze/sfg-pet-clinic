3621e08e411c42b1abfbf939fd9a3e9250a22e14
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-22 16:00:24 -0500

    Refactored service layer and added Crud service to consolidate duplicate methods to one shared service. Closes #14

--- pet-clinic-data/src/main/java/austinspinazze/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/austinspinazze/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package austinspinazze.springframework.sfgpetclinic.services;
 
 import austinspinazze.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
